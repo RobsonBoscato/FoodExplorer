1e9e655bc0b63acb5de7d3b5818fb249bada697c
Robson Boscato <rbsn.boscato@gmail.com>
2023-09-12 10:04:20 -0300

    created Routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,15 @@ import ReactDOM from "react-dom/client";
 import GlobalStyle from "./pages/Styles/GlobalStyle";
 import { ThemeProvider } from "styled-components";
 
-import { SignIn } from "./pages/SignIn";
-import { SignUp } from "./pages/SignUp";
-import { Home } from "./pages/Home";
-import { Details } from "./pages/Details";
-import { Menu } from "./pages/Menu";
+import { Routes } from "./routes";
+
 import theme from "./pages/Styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
-			<Menu />
+			<Routes />
 		</ThemeProvider>
 	</React.StrictMode>
 );
